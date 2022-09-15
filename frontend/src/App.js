8bd29fec090ba208dcb5081ba288fef15681072f
Ketan bhoir <96060809+ketanbhoir@users.noreply.github.com>
2022-09-16 03:16:52 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
+ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/"> Flipmart</a>
-      </header>
-
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Flipmart</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
